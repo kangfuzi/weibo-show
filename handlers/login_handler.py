eb4e5a4cdc3f0167bdb4e5b2ad1d45207c72af87
kangfuzi <wangtao@kangfuzi.cn>
2016-02-24 16:33:51 +0800

    测试

--- handlers/login_handler.py
+++ handlers/login_handler.py
@@ -9,6 +9,8 @@ class LoginHandler(BaseHandler):
     def get(self, action):
         if action == "":
             self.signin_page()
+        elif action == "weibo":
+            self.weibo_signin()
         elif action == "signout":
             self.signout()
         elif action == "register":
@@ -27,6 +29,11 @@ class LoginHandler(BaseHandler):
     def signin_page(self):
         self.render('signin.html')
 
+    def weibo_signin(self):
+        url = 'https://api.weibo.com/oauth2/authorize?client_id=YOUR_CLIENT_ID&response_type=code&redirect_uri=YOUR_REGISTERED_REDIRECT_URI'
+        self.redirect(url)
+
+        'https://api.weibo.com/oauth2/access_token?client_id=YOUR_CLIENT_ID&client_secret=YOUR_CLIENT_SECRET&grant_type=authorization_code&redirect_uri=YOUR_REGISTERED_REDIRECT_URI&code=CODE'
     def signout(self):
         self.set_secure_cookie('uid', "")
 
